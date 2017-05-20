f5e7debca74382d0f83d0521c455209e90c59311
elbohairy <totski@gmail.com>
2017-05-20 13:37:33 -0400

    completed the basic structure of generating moves. Need to refactor and code queue idea.

--- knights_travails.rb
+++ knights_travails.rb
@@ -73,13 +73,28 @@ def knight_moves position, destination
     # So how should we use map?
       # A: by using conditionals in the block. I suppose if we have a move
       # that isn't valid, we could use next to skip it?
-  valid_moves = cases.map do 
+  valid_moves = cases.map do |x, y|
+    next_move = [ position[0] + x, position[1] + y ]
+    if board.valid_move? next_move
+      next_move
+    else
+      next
+    end
+  end
 
+  valid_moves.compact!
+  p valid_moves
 
 
 
   # then we initialize a bunch of knights with those positions, and pass them as children to 
   # the initial knight.
+
+  valid_moves.each do |position|
+    root_knight.moves.push Knight.new position
+  end
+
+  p root_knight.inspect
 
   # THEN, we have a problem.
     # How do we then add children to those childreN?
@@ -88,8 +103,59 @@ def knight_moves position, destination
         # we can simply loop through each item (knight) in the array.
         # and for each knight we'll just do again what we did to the first knight.
 
+  current_knight = root_knight
+
+  counter = 0
+  until counter == 7
+    current_knight.moves.each do |move|
+
+      # need to create an array of valid moves for that knight
+      valid_moves = cases.map do |x, y|
+        next_move = [ move.position[0] + x, move.position[1] + y ]
+        if board.valid_move? next_move
+          next_move
+        else
+         next
+        end
+      end
+
+      # then remove all nil values
+      valid_moves.compact!
+      # THEN push instances of knights with those positions (from the arrays) to the knight
+      valid_moves.each do |position|
+        move.moves.push Knight.new position
+      end
+
+    end
+
+
+    # right here, I need to find some way of changing root_knight to its children, then
+    # to the children's children, until some arbitrary point.
+    # But I don't see the obvious way to do that.
+    # Refactoring may help with that (lots of duplication)
+
+    # POSSIBLE SOLUTION:
+      # use a queue. Just keep adding to the queue until some arbitrary point
+      # implementation?
+        # I have no idea
+
+  end
+
+
+  p 'SPAGHETTI'
+  p root_knight
+
+
+  # FINISHED basic structure.
+    # Now just need to figure out how to loop it properly.
+      # e.g., current knight will need to change every iteration
+
+
+
+
+
     # How do we stop this from going on forever?
       # A: we could just arbitrarily limit how many times we do this whole process
 end
 
-p knight_moves [5, 2], [2, 6]
+p knight_moves [7, 7], [2, 6]
